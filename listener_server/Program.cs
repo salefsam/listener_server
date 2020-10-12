92c3195d99d0502577d38fd258a7e5112b5083bb
Sam Salefske <Sam.salefske@gmail.com>
2020-10-11 21:10:07 -0400

    Setting up project

--- listener_server/Program.cs
+++ listener_server/Program.cs
@@ -7,6 +7,7 @@ namespace listener_server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
---
+++ listener_server/common/AppConfigHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace listener_server.common
+{
+    class AppConfigHelper
+    {
+    }
+}
